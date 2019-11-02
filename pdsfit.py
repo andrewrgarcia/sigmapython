3cef67fb4b3bec2f317a1133a19f9fdd26cfe071
Andrew Garcia <garcia.gtr@gmail.com>
2019-11-02 18:49:02 -0400

    improvements; decreased from 264 to 200 lines of code

--- pdsfit.py
+++ pdsfit.py
@@ -43,7 +43,8 @@ def lastRow(idx, workbook, col=1):
     return lwr_cell.row
 
 
-def make(data,name,pds=['gauss','lognorm','expon','gamma','beta'],plots=True,bins= 15,xlims=[0,400]):
+def make(data,name,pds=['gauss','lognorm','expon','gamma','beta'],\
+         plots=True,bins= 15,xlims=[0,400]):
         
     plt.figure() if plots is True else None
 
@@ -52,7 +53,6 @@ def make(data,name,pds=['gauss','lognorm','expon','gamma','beta'],plots=True,bin
     
     n, bins, patches = plt.hist(data,bins = bins,stacked =True, weights=wts,\
                                 color='dodgerblue',edgecolor='k',linewidth=1.2)
-
     
     plt.xlim(xlims) if xlims != '' else None
     
@@ -70,134 +70,71 @@ def make(data,name,pds=['gauss','lognorm','expon','gamma','beta'],plots=True,bin
     'save stats -- labels and values'
     stats_lbl = []
     stats_val = []
-    stats_lbl.append('sample_name')
-    stats_val.append(name)
-    stats_lbl.append('sample_size')
-    stats_val.append(size(data))
 
+
+    def fit(typedist):
     
-    '''*NORMAL DISTRIBUTION (GAUSSIAN)'''
-    if 'gauss' in pds:
-        m, s = stats.norm.fit(data) # get mean and standard deviation  
+        if typedist == 'gauss':
+            distfit,statspdf = stats.norm.fit,stats.norm.pdf
+            labels,pltlbl = ['normal_mean', 'normal_sdev'], 'normal'
+            
+        elif typedist == 'lognorm':
+            distfit,statspdf = stats.lognorm.fit,stats.lognorm.pdf
+            labels,pltlbl = ['lognorm_s/sigma', 'lognorm_loc','lognorm_scale/median/exp_mean'], 'lognormal'
+            
+            '''parametrization
+            https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.lognorm.html
+            https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.lognorm.html
+            mean = np.log(scale)
+            sigma = s
+            median = scale '''
+            
+        elif typedist == 'expon':
+            distfit,statspdf = stats.expon.fit,stats.expon.pdf
+            labels,pltlbl = ['expon_loc','expon_scale'], 'exponential'
         
+        elif typedist == 'gamma':
+            distfit,statspdf = stats.gamma.fit,stats.gamma.pdf
+            labels,pltlbl = ['gamma_a','gamma_b','gamma_c'], 'gamma'
+        elif typedist == 'beta':
+            distfit,statspdf = stats.beta.fit,stats.beta.pdf
+            labels,pltlbl = ['beta_a','beta_b','beta_c','beta_d'], 'beta'
+        
+        
+        pars = distfit(data)
         'scale "normalized" pdf to normalized bins'
-        max_pdf = np.max(stats.norm.pdf(lspc, m, s))
-        pdf_g = stats.norm.pdf(lspc, m, s)  * (max_normbins/max_pdf)     
-    #    plt.plot(lspc, pdf_g,label="Normal " + name) # plot it
+        max_pdf = np.max(statspdf(lspc, *pars))
+        pdf = statspdf(lspc, *pars)  * (max_normbins/max_pdf)     
     
-        plt.plot(lspc, pdf_g,color='purple',label='normal') if plots is True else None
+        plt.plot(lspc, pdf,label=pltlbl) if plots is True else None
     
-    #    print(name, 'distribution fit statistics')
         print('')
         print(name)
+        
+        for i in range(len(labels)):
+            print(labels[i],pars[i])
+        
+        for i in range(len(labels)):
+            stats_lbl.append(labels[i]), stats_val.append(pars[i])
+        
+#        return labels, pars
     
-    
-        print('sample_size \n {}'.format(size(data)))
-        print('\n normal: \n \
-              mean sdev \n \
-              {} {}'.format(np.round(m,2),np.round(s,2)))
-        
-        stats_lbl.append('normal_mean'), stats_val.append(m)
-        stats_lbl.append('normal_sdev'), stats_val.append(s)
 
-
-#        print('normal: ' ,'mean', np.round(m,2),'std_dev', np.round(s,2))
-    
-    '''*LOGNORMAL DISTRIBUTION'''
-    if 'lognorm' in pds:
-
-        s, loc, scale = stats.lognorm.fit(data) # get mean and standard deviation 
-        '''parametrization'
-    #    https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.lognorm.html
-    #    https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.lognorm.html'''
-        mean = np.log(scale)
-        sigma = s
-#        mean, var, skew, kurt = stats.lognorm.stats(s, moments='mvsk')
-        
-        median = scale
-        
-        'scale normalized pd to bins'
-        max_pdf = np.max(stats.lognorm.pdf(lspc, s,loc,scale))
-        pdf_logn = stats.lognorm.pdf(lspc, s,loc,scale) * (max_normbins/max_pdf)
-        
-        plt.plot(lspc, pdf_logn,color='gold',label="lognormal") if plots is True else None
-        
-        plt.xlabel('Length  /  $\mu m$')
-        
-        print('\n lognormal: \n \
-              s loc scale mean median \n \
-              {} {} {} {} {} \n'.format(np.round(s,2),np.round(loc,2), np.round(scale,2),mean,median))
-
-        stats_lbl.append('lognorm_s'), stats_val.append(s)
-        stats_lbl.append('lognorm_loc'), stats_val.append(loc)
-        stats_lbl.append('lognorm_scale'), stats_val.append(scale)
-        stats_lbl.append('lognorm_mean'), stats_val.append(mean)
-        stats_lbl.append('lognorm_median'), stats_val.append(median)
-
-#        print('\n lognormal: \n s {} loc {} scale {} \
-#              \n mean = {} \n median = {} \n mode = {} \
-#              \n '.format(np.round(s,2),np.round(loc,2), np.round(scale,2),mean,median,mode))
-
-    '''*EXPONENTIAL DISTRIBUTION'''
-    if 'expon' in pds:
-        loc, scale = stats.expon.fit(data)
-        pdf_expon = stats.expon.pdf(lspc, loc, scale)  
-        'scale normalized pd to bins'
-        max_pdf = np.max(stats.expon.pdf(lspc, loc,scale))
-        pdf_expon = stats.expon.pdf(lspc, loc,scale)  * (max_normbins/max_pdf)   
-        
-        plt.plot(lspc, pdf_expon,color='crimson',label="expon") if plots is True else None
-        print('\n exponential: \n loc {} scale {} \
-              \n '.format(loc,scale))
-        
-        stats_lbl.append('expon_loc'), stats_val.append(loc)
-        stats_lbl.append('expon_scale'), stats_val.append(scale)
-        
-        
-    '''*GAMMA DISTRIBUTION'''
-    if 'gamma' in pds:
-
-        ag,bg,cg = stats.gamma.fit(data)  
-        pdf_gamma = stats.gamma.pdf(lspc, ag, bg,cg)  
-        'scale normalized pd to bins'
-        max_pdf = np.max(stats.gamma.pdf(lspc, ag,bg,cg))
-        pdf_gamma = stats.gamma.pdf(lspc, ag,bg,cg)  * (max_normbins/max_pdf)   
-        
-        plt.plot(lspc, pdf_gamma,label="gamma") if plots is True else None
-        print('Gamma: ' ,'aG', np.round(ag,2),'bG', np.round(bg,2),\
-              'cG', np.round(cg,2))
-        
-        stats_lbl.append('gamma_a'), stats_val.append(ag)
-        stats_lbl.append('gamma_b'), stats_val.append(bg)
-        stats_lbl.append('gamma_c'), stats_val.append(cg)
+    fit('gauss') if 'gauss' in pds else None
+    fit('lognorm') if 'lognorm' in pds else None
+    fit('expon') if 'expon' in pds else None
+    fit('gamma') if 'gamma' in pds else None
+    fit('beta') if 'beta' in pds else None
 
     
-    '''*BETA DISTRIBUTION'''
-    if 'beta' in pds:
-
-        ab,bb,cb,db = stats.beta.fit(data)  
-        pdf_beta = stats.beta.pdf(lspc, ab, bb,cb, db)  
-    #    plt.plot(lspc, pdf_beta, label="Beta " + name)
-        'scale normalized pd to bins'
-        max_pdf = np.max(stats.beta.pdf(lspc, ab,bb,cb,db))
-        pdf_beta = stats.beta.pdf(lspc, ab,bb,cb,db)  * (max_normbins/max_pdf)   
-        
-        plt.plot(lspc, pdf_beta,label="beta") if plots is True else None
-        print('Beta: ' ,'aB', np.round(ab,2),'bB', np.round(bb,2),\
-              'cB', np.round(cb,2),'dB', np.round(db,2))
-        print()
-        
-        stats_lbl.append('beta_a'), stats_val.append(ab)
-        stats_lbl.append('beta_b'), stats_val.append(bb)
-        stats_lbl.append('beta_c'), stats_val.append(cb)
-        stats_lbl.append('beta_d'), stats_val.append(db)
+    print(stats_lbl)
+    print(stats_val)
 
 
     plt.title(name) if plots is True else None
     plt.legend() if plots is True else None
-    
+    plt.show()
     return stats_lbl, stats_val
-
 
 
 '''    EXECUTION   '''
@@ -211,10 +148,11 @@ def single():
     book, label = excelbook('SEM',mysample())
     idx = 'Results'
     diam =   book.sheets[idx].range( 'I2:I'+str(lastRow(idx,book)) ).value 
-    make(diam,label+' (Diameter)',['gauss','lognorm'],bins=100,plots=True,xlims='')
+#    make(diam,label+' (Diameter)',['gauss','lognorm'],bins=100,plots=True,xlims='')
+    make(diam,label+' (Diameter)',['lognorm'],bins=100,plots=True,xlims='')
     book.close()
 
-single()
+#single()
 
 def batch(toexcel=True,plots=True):
     
@@ -250,16 +188,13 @@ def batch(toexcel=True,plots=True):
 
 
 '''EXAMPLES'''
-#ex1 = np.random.normal(10, 10, 1000)
-#ex2 = 2*np.random.uniform(1,40, 1000) + np.random.normal(10, 10, 1000)
-#ex3 = 3*np.random.exponential(4,1000)
+ex1 = np.random.normal(10, 10, 1000)
+ex2 = 2*np.random.uniform(1,40, 1000) + np.random.normal(10, 10, 1000)
+ex3 = 3*np.random.exponential(4,1000)
 
-#
-#make(ex1,'example 1',['gamma','beta'],20)
-#make(ex2,'example 2',['gauss'],20)
-#make(ex3,'example 3',bins=20)
+
+make(ex1,'example 1',['gamma','beta'],bins=20,xlims='')
+make(ex2,'example 2',['gauss'],bins=20,xlims='')
+make(ex3,'example 3',bins=20,xlims='')
 
 '''------------------------------------------------------------------------------------'''
-
-
-
